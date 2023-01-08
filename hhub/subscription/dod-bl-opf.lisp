8204906c5338ea448e3351e4cd8ffd63189e511d
HighriseHub Developer <support@highrisehub.com>
2023-01-08 04:34:32 +0000

    Changed the name of the database table

--- hhub/subscription/dod-bl-opf.lisp
+++ hhub/subscription/dod-bl-opf.lisp
@@ -18,13 +18,13 @@
 		[=[:row-id] id]]    :caching *dod-database-caching* :flatp t ))))
 
 (defun get-opreflist-for-customer (customer &optional (pdbcaching nil))
-(let ((tenant-id (slot-value customer 'tenant-id))
+  (let ((tenant-id (slot-value customer 'tenant-id))
 	(cust-id (slot-value customer 'row-id)))
-(clsql:select 'dod-ord-pref  :where
-		[and [= [:deleted-state] "N"]
-		[= [:tenant-id] tenant-id]
-		[=[:cust-id] cust-id ]]
-		:caching pdbcaching :flatp t )))
+    (clsql:select 'dod-ord-pref  :where
+		  [and [= [:deleted-state] "N"]
+		  [= [:tenant-id] tenant-id]
+		  [=[:cust-id] cust-id ]]
+				 :caching pdbcaching :flatp t )))
     
 
 
