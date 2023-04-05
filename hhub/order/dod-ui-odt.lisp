c43be8a2796038e384416b914c8982e67ca4c837
HighriseHub Developer <support@highrisehub.com>
2023-04-05 23:54:23 +0000

    Using the html-table macro for the emailing the order data

--- hhub/order/dod-ui-odt.lisp
+++ hhub/order/dod-ui-odt.lisp
@@ -115,10 +115,9 @@
 
 (defun ui-list-shopcart-for-email (products shopcart)
     :documentation "A function used for rendering the shopping cart data in HTML EMAIL format."
-    (cl-who:with-html-output (*standard-output* nil)
-      (:table :width "100%" :border "1" :align "center" :cellpadding "0" :cellspacing "0" :style "padding: 0; margin: 0;"
-	      (mapcar (lambda (product odt)
-			(product-card-for-email product odt))  products shopcart ))))
+  (with-html-table "" (list "Product" "Description" "Qty" "Subtotal") "1"  
+    (mapcar (lambda (product odt)
+	      (product-card-for-email product odt))  products shopcart )))
     
 
 (defun ui-list-cust-orderdetails  (header data)
