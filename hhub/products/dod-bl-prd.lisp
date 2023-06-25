bd1e12a303e95f5c025be0fd907be1b990523acf
HighriseHub Developer <support@highrisehub.com>
2023-06-25 18:05:03 +0000

    Removed some methods from here and moved to another file

--- hhub/products/dod-bl-prd.lisp
+++ hhub/products/dod-bl-prd.lisp
@@ -2,39 +2,11 @@
 (in-package :hhub)
 (clsql:file-enable-sql-reader-syntax)
 
-(defun generate-product-ext-url (product)
-  :description "Generates an external URL for a product, which can be shared with external entities"
-  (let* ((tenant-id (slot-value product 'tenant-id))
-	 (prd-id (slot-value product 'row-id))
-	 (param-csv (format nil "tenant-id,product-id~C~A,~A" #\linefeed tenant-id prd-id))
-	 (param-base64 (cl-base64:string-to-base64-string param-csv)))
-    (format nil "~A/hhub/hhubprddetailsforguestcust?key=~A" *siteurl* param-base64)))
-		    
-
-(defun approve-product (id description company)
-  (let ((product (select-product-by-id id company)))
-    (if product 
-	(progn (setf (slot-value product 'approved-flag) "Y")
-	       (setf (slot-value product 'approval-status) "APPROVED")
-	       (setf (slot-value product 'description) description)
-	       (update-prd-details product)))))
-
-(defun reject-product (id description company)
-  (let ((product (select-product-by-id id  company)))
-    (if product 
-	(progn (setf (slot-value product 'approved-flag) "N")
-	       (setf (slot-value product 'approval-status) "REJECTED")
-	       (setf (slot-value product 'description) description)
-	       (update-prd-details product)))))
 
 (defun deactivate-product (id company)
   (let ((product (select-product-by-id id company)))
     (setf (slot-value product 'active-flag) "N")
     (update-prd-details product)))
-
-
-
-
 
 (defun activate-product (id company)
   (let ((product (select-product-by-id id company)))
@@ -51,6 +23,8 @@
 		[= [:tenant-id] tenant-id]
 		[= [:approval-status] "PENDING"]]
 		:caching *dod-database-caching* :flatp t ))
+
+
 
 (defun get-products-for-approval-by-company (tenant-id)
   :documentation "This function will be used by the company administrator"
