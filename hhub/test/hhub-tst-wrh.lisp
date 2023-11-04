cadfc1d9066520ebd1bfce1476a3255d1db63327
HighriseHub Developer <support@highrisehub.com>
2023-11-04 22:59:18 +0530

    Added test cases for warehouse testing

--- hhub/test/hhub-tst-wrh.lisp
+++ hhub/test/hhub-tst-wrh.lisp
@@ -1,9 +1,31 @@
 ;; -*- mode: common-lisp; coding: utf-8 -*-
 (in-package :hhub)
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmacro with-entity-create (adaptername requestmodel &body body)
+    :description "Creates a business entity when provided with a adapter name and requestmodel"
+    `(let* ((adapter (make-instance ,adaptername)))
+       ;; create the entity in the Database and return back the business object. 
+       (ProcessCreateRequest adapter ,requestmodel)
+       ,@body)))
 
-(defun test-warehouse-DBSave ()
-;; (handler-case   
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmacro with-entity-readall (adaptername requestmodel &body body)
+    `(let* ((adapter (make-instance ,adaptername)))
+      ;; Read the entity from the database and return back a list of business objects
+      (ProcessReadallRequest adapter ,requestmodel)
+      ,@body)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmacro with-entity-read (adaptername requestmodel &body body)
+    `(let* ((adapter (make-instance ,adaptername)))
+       ;; Read the entity from the database adn return back a single business object.
+       (ProcessReadRequest adapter ,requestmodel)
+       ,@body)))
+
+
+
+(defun test-warehouse-DBSave (managername phone)
   (let* ((democompany (select-company-by-id 2))
 	 (requestmodel (make-instance 'WarehouseRequestModel
 				      :w-name  "Test Warehouse"
@@ -13,11 +35,26 @@
 				      :w-city "Bengaluru"
 				      :w-state "Karnataka"
 				      :w-country "IN"
-				      :w-manager "Pawan Deshpande"
-				      :w-phone "9999339933"
+				      :w-manager managername
+				      :w-phone phone
 				      :w-alt-phone "9393993222"
 				      :w-email "warehouse@example.com"
-				      :company democompany))
-	    (warehouseadapter (make-instance 'WarehouseAdapter)))
-	 
-       (ProcessCreateRequest warehouseadapter requestmodel)))
+				      :company democompany)))
+    (with-entity-create 'WarehouseAdapter requestmodel)))
+
+
+(defun test-warehouses-fetch () 
+  (let* ((company (select-company-by-id 2))
+	 (requestmodel (make-instance 'WarehouseRequestModel)))
+    (setf (slot-value requestmodel 'company) company)
+    (with-entity-readall 'WarehouseAdapter requestmodel)))
+
+(defun test-warehouse-fetch () 
+  (let* ((company (select-company-by-id 2))
+	 (requestmodel (make-instance 'WarehouseRequestModel)))
+
+    (setf (slot-value requestmodel 'company) company)
+    (setf (slot-value requestmodel 'w-phone) "9999339933")
+    (with-entity-readall 'WarehouseAdapter requestmodel
+    (with-entity-read 'WarehouseAdapter requestmodel))))
+    
