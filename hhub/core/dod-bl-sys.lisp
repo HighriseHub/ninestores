3addedac2d406cff80d5258f2d14a26b42b90854
HighriseHub Developer <support@highrisehub.com>
2023-12-30 19:47:11 +0530

    Added function to fetch currencies and store in memory while the app is running

--- hhub/core/dod-bl-sys.lisp
+++ hhub/core/dod-bl-sys.lisp
@@ -6,3 +6,18 @@
 (defun refreshiamsettings ()
   (setf *HHUBGLOBALLYCACHEDLISTSFUNCTIONS* (hhub-gen-globally-cached-lists-functions)))
 
+(defun get-system-currencies-ht ()
+  :documentation "This function stores all the currencies in a hashtable. The Key = country, Value = list of currency, code and symbol."
+  (let ((ht (make-hash-table :test 'equal))
+	(currencies (clsql:select 'dod-currencies    :caching *dod-database-caching* :flatp t )))
+    (loop for curr in currencies do
+      (let ((key (slot-value curr 'country))
+	    (currency (slot-value curr 'currency))
+	    (code (slot-value curr 'code))
+	    (symbol (slot-value curr 'symbol)))
+	   (setf (gethash key ht) (list currency code symbol))))
+    ; Return  the hash table. 
+    ht))
+
+
+
