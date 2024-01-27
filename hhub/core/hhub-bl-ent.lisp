f9f066a5b9af12df4149999167b85af69f39cdb3
HighriseHub Developer <support@highrisehub.com>
2024-01-27 09:56:11 +0530

    created entity to return from any processxxxx method

--- hhub/core/hhub-bl-ent.lisp
+++ hhub/core/hhub-bl-ent.lisp
@@ -569,22 +569,29 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defmacro with-entity-create (adaptername requestmodel &body body)
     :description "Creates a business entity when provided with a adapter name and requestmodel"
-    `(let* ((adapter (make-instance ,adaptername)))
+    `(let* ((adapter (make-instance ,adaptername))
+	    (entity (ProcessCreateRequest adapter ,requestmodel)))
        ;; create the entity in the Database and return back the business object. 
-       (ProcessCreateRequest adapter ,requestmodel)
        ,@body)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defmacro with-entity-readall (adaptername requestmodel &body body)
-    `(let* ((adapter (make-instance ,adaptername)))
+    `(let* ((adapter (make-instance ,adaptername))
+	    (allentities (ProcessReadallRequest adapter ,requestmodel)))
       ;; Read the entity from the database and return back a list of business objects
-      (ProcessReadallRequest adapter ,requestmodel)
-      ,@body)))
+       ,@body)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defmacro with-entity-read (adaptername requestmodel &body body)
-    `(let* ((adapter (make-instance ,adaptername)))
+    `(let* ((adapter (make-instance ,adaptername))
+	    (entity (ProcessReadRequest adapter ,requestmodel)))
        ;; Read the entity from the database adn return back a single business object.
-       (ProcessReadRequest adapter ,requestmodel)
+       ,@body)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmacro with-entity-update (adaptername requestmodel &body body)
+    `(let* ((adapter (make-instance ,adaptername))
+	    (entity (ProcessUpdateRequest adapter ,requestmodel)))
+       ;; Read the entity from the database adn return back a single business object.
        ,@body)))
 
