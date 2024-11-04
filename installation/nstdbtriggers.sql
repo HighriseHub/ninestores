b201a1ac0a828e455c520b158bb06d4ca2b73b79
HighriseHub Developer <support@highrisehub.com>
2024-11-04 23:32:44 +0530

    Changed the invoice number to use a - instead of /

--- installation/nstdbtriggers.sql
+++ installation/nstdbtriggers.sql
@@ -5,6 +5,6 @@ create trigger before_insert_invoice before insert on DOD_INVOICE_HEADER for eac
     begin 
        	     declare next_id mediumint;
 	     select auto_increment from information_schema.tables where table_schema = 'hhubdb' and table_name = 'DOD_INVOICE_HEADER' into next_id;
-    	     set NEW.INVNUM = concat ('NST',lpad(next_id,5,"0"), "/",year(now()));
+    	     set NEW.INVNUM = concat ('NST',lpad(next_id,5,"0"), "-",year(now()));
     end;//
 delimiter ;
